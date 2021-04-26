77310502a4f2ef8800a8532772031cb4d9ab55ce
inefix <inefix3000@hotmail.com>
2021-04-26 12:08:07 +0000

    bug fix

--- HomeNetworkServer/frontend/src/components/Navigation.jsx
+++ HomeNetworkServer/frontend/src/components/Navigation.jsx
@@ -7,7 +7,7 @@ function Navigation(props) {
       <nav class="navbar navbar-expand navbar-dark bg-dark">
         <div class="container">
           <Link class="navbar-brand" to="/">
-            React Multi-Page Website
+            Decentralized LoRa infrastructure using blockchain
           </Link>
 
           <div>
