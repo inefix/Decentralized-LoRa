90c8fbd8b2a40067c46261e7b04a448f87a689b1
inefix <inefix3000@hotmail.com>
2021-04-29 16:37:13 +0200

    process the received data

--- ForwardingNetworkServer/server.py
+++ ForwardingNetworkServer/server.py
@@ -2,12 +2,65 @@
 # https://gist.github.com/vxgmichel/b2cf8536363275e735c231caef35a5df
 
 import asyncio
+import base64
 
 local_addr = "0.0.0.0"
 local_port = 1680
 
 remote_host = "163.172.130.246"
 remote_port = 9999
+
+
+async def process(data) :
+    size = len(data)
+    #print(data[3])
+    print(f'{data} {type(data)} {size}')
+
+    if size < 12 :
+        print(" (too short for GW <-> MAC protocol)\n")
+        return b'error'
+    else :
+        print("Process the data 1")
+        if data[0] != 2 :
+            print("invalid version\n")
+            return b'error'
+        else :
+            print("Process the data 2")
+            if data[3] != 0 :
+                print("Not the right gateway command")
+                return b'error'
+            else :
+                print("Process the data 3")
+                #print(data[len(data)-10])
+                string = str(data)
+                if "data" not in string :
+                    print("No data field")
+                    return b'error'
+                else :
+                    print("Process the data 4")
+                    #print(string)
+                    tableau = string.split("{")
+                    #print(tableau)
+                    tableau[2] = tableau[2][:-2]
+                    #print(tableau[2])
+                    tableau = tableau[2].split(",")
+                    nice_data = tableau[len(tableau)-1]
+                    #print(nice_data)
+                    if "data" not in nice_data :
+                        print("No data field")
+                        return b'error'
+                    else :
+                        print("Process the data 5")
+                        nice_data = nice_data.split(":")
+                        #print(nice_data[1])
+                        final = nice_data[1][1:]
+                        final = final[:-1]
+                        print(final)
+                        print(base64.b64decode(final))
+
+                        return final
+
+
 
 class ProxyDatagramProtocol(asyncio.DatagramProtocol):
 
@@ -20,17 +73,29 @@ class ProxyDatagramProtocol(asyncio.DatagramProtocol):
         self.transport = transport
 
     def datagram_received(self, data, addr):
-        #print('Received \x1b[0;30;46m%s\x1b[0m from %s' % (data, addr))
-        print("Received from device :", data)
-        if addr in self.remotes:
-            self.remotes[addr].transport.sendto(data)
-            return
         loop = asyncio.get_event_loop()
-        #print("Device addr :", addr)
-        self.remotes[addr] = RemoteDatagramProtocol(self, addr, data)
-        coro = loop.create_datagram_endpoint(
-            lambda: self.remotes[addr], remote_addr=self.remote_address)
-        asyncio.ensure_future(coro)
+        loop.create_task(self.datagram_received_async(data, addr)) 
+
+    async def datagram_received_async(self, data, addr):
+        #print('Received \x1b[0;30;46m%s\x1b[0m from %s' % (data, addr))
+        #print("Received from device :", data)
+        processed = await process(data)
+        if processed != b'error':
+            # a = bytearray(data)
+            # a[3] = 4
+            # data = bytes(a)
+            # print(data)
+            # self.transport.sendto(data, addr)
+            # print("data sent to :", addr)
+            if addr in self.remotes:
+                self.remotes[addr].transport.sendto(data)
+                return
+            loop = asyncio.get_event_loop()
+            #print("Device addr :", addr)
+            self.remotes[addr] = RemoteDatagramProtocol(self, addr, data)
+            coro = loop.create_datagram_endpoint(
+                lambda: self.remotes[addr], remote_addr=self.remote_address)
+            asyncio.ensure_future(coro)
 
 
 class RemoteDatagramProtocol(asyncio.DatagramProtocol):
