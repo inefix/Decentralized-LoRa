3900add632e72ccb70d29032a9195ed59904ae47
inefix <inefix3000@hotmail.com>
2021-07-26 17:07:53 +0200

    lora_device

--- lora_device/device.py
+++ lora_device/device.py
@@ -1,38 +1,24 @@
 import asyncio
+# pip3 install pyserial-asyncio
+from serial_asyncio import open_serial_connection
 
-import os
-import zlib
-import random
-#import numpy as np
 import json
 
 # pip3 install cryptography
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.exceptions import InvalidSignature
-#from cbor2 import dumps, loads
 
 from binascii import unhexlify, hexlify
 
-# pip3 install cose
-from cose.messages import Sign1Message, CoseMessage, Enc0Message, Mac0Message, Countersign0Message
-from cose.keys import CoseKey, EC2Key, SymmetricKey
+from cose.messages import CoseMessage, Enc0Message, Countersign0Message
+from cose.keys import CoseKey, SymmetricKey
 from cose.headers import Algorithm, KID, IV, Reserved
-from cose.algorithms import EdDSA, Es256, EcdhEsA256KW, EcdhEsA128KW, DirectHKDFAES128, EcdhSsA128KW, A128GCM, HMAC256
-from cose.keys.keyparam import KpKty, OKPKpD, OKPKpX, KpKeyOps, OKPKpCurve, EC2KpX, EC2KpY, KpAlg, KpKty, EC2KpD, EC2KpX, KpKeyOps, EC2KpCurve, EC2KpY, KpKid, SymKpK
-from cose.keys.keytype import KtyEC2, KtySymmetric, KtyOKP
-from cose.keys.keyops import SignOp, VerifyOp, DeriveKeyOp, MacCreateOp, MacVerifyOp
+from cose.algorithms import Es256, A128GCM
+from cose.keys.keyparam import EC2KpX, EC2KpY, EC2KpX, EC2KpY
 
-add = "163.172.130.246"
-port = 9999
-
-# add = "0.0.0.0"
-# port = 1680
 
 deviceAdd = "0x1145f03880d8a975"
 serialized_private = b'-----BEGIN PRIVATE KEY-----\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgMSp/hxGyOMubQVr5\nxIUYeVqFjylWXBNRjvyp1di865ChRANCAARkOGbAJWUYFw8k6PsBsjM/1+8ULqrg\nmqjBIrQbkGY9DNTdZDcQOtvOg8dXiPN25nu4q3/Mda7pMyvSCSB3I7Jv\n-----END PRIVATE KEY-----\n'
@@ -49,7 +35,29 @@ serialized_public_server = b'-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZ
 # message = b'\xd2\x84C\xa1\x01&\xa0Xr\xd0\x83XF\xa3\x01\x01\x05X\x1a000102030405060708090a0b0c\x00x#["0100", "0", "0x37dae2a4323e7028"]\xa0X%-b\xc8}l\xc6\x91#\x1bm?\x06\xd2\x13\x13ac\xd8\xa0@\xec\xa2\xc2!\xb0\xd3K)\x9b\xcc\xf1\x95\xaes\x19\xaa\xe7X@\xf4\x11\x1ayj\x1f\'\xf1/\xe8\x8fi6\x0eO\x95\xccE\xff\xb8\xbc7-ff\xf1\xdc\xf5m\xac\xf0qsH\x95\xc5\xc94\xe7\xae@\xab\x8f\x13\xa6u\xd9\xcf\xdc\xa0\xd4\x86\xa9\x88\xa3\xa2\x92\xc4m\xe3\x1a\xf4\xbf\xb0'
 # Counter sign :
 # message = b'\xd0\x83XF\xa3\x01\x01\x05X\x1a000102030405060708090a0b0c\x00x#["0011", "0", "0x1145f03880d8a975"]\xa1\x0b\x83C\xa1\x01&\xa0X@\x92\xc9\xb2v\xa4\xaa\xd3s+\x8a\xebT\xdc\x07o\xf5NH\xe8 Rz4\x82\xe3H\x18\x97\x15\xb6Z\x1c\x97$X\\H\xd8\x88/aC\x15\x9d\x07\x93\x1ftG\xd1\xa2T\xb5\x9eB\xe0^J\xcb\x82\xd8\xccN\rUk\xe5(J\x13\rz\xf3\xf7\xbe\xb3\xa7\xc4\x88\xc1\xe9\xbf\xaaM\xc8i'
-message = "Hello"
+# message = "Hello"
+
+async def read_increment_counter():
+    try :
+        # read the counter
+        f = open("counter.txt", "r")
+        counter = f.read()
+        f.close()
+    except FileNotFoundError:
+        counter = ""
+
+    if counter == "" :
+        counter = 0
+    else :
+        counter = int(counter) + 1
+    # print(counter)
+    # update the counter
+    f = open("counter.txt", "w")
+    f.write(str(counter))
+    f.close()
+
+    return counter
+
 
 async def generate_key_sym():
     privkey = serialization.load_pem_private_key(serialized_private, password=None, backend=default_backend())
@@ -73,7 +81,7 @@ async def generate_key_sym():
 
 async def encrypt(text, key):
     d = {
-        "0000": "JoinRequest", 
+        "0000": "JoinRequest",
         "0001": "JoinResponse",
         "0010": "JoinAccept",
         "0011": "DataConfirmedUp",
@@ -88,9 +96,9 @@ async def encrypt(text, key):
     #print(get_key_by_value(d, reverse, "JoinResponse"))
 
     pType = get_key_by_value(d, reverse, "DataConfirmedUp")
-    counter = "0"
+    counter = await read_increment_counter()
     plaintext = text
-    header = [pType, counter, deviceAdd]
+    header = [pType, str(counter), deviceAdd]
 
     msg = Enc0Message(
         phdr = {Algorithm: A128GCM, IV: b'000102030405060708090a0b0c', Reserved: json.dumps(header)},
@@ -108,71 +116,13 @@ async def encrypt(text, key):
     return encrypted
 
 
-# COSE Sign1 version
-# async def sign(encrypted):
-#     privkey = serialization.load_pem_private_key(serialized_private, password=None, backend=default_backend())
-#     bytes_key_priv = privkey.private_numbers().private_value.to_bytes(32, 'big')
-#     x = format(privkey.private_numbers().public_numbers.x, '064x')
-#     y = format(privkey.private_numbers().public_numbers.y, '064x')
-    
-#     msg2 = Sign1Message(
-#         phdr = {Algorithm: Es256},
-#         #payload = 'signed message'.encode('utf-8')
-#         payload = encrypted
-#     )
-
-#     key_attribute_dict = {
-#         'KTY': 'EC2',
-#         'CURVE': 'P_256',
-#         'ALG': 'ES256',
-#         EC2KpX : unhexlify(x),
-#         EC2KpY : unhexlify(y),
-#         'D': bytes_key_priv
-#     }
-#     cose_key = CoseKey.from_dict(key_attribute_dict)
-#     #print(cose_key)
-
-#     msg2.key = cose_key
-#     packet = msg2.encode()
-#     print("Packet :", packet)
-#     #print("Packet hexlify :", hexlify(packet))
-
-#     return packet
-
-
-# COSE Sign1 version
-# async def check_signature(packet, pubkey):
-#     pubkey = serialization.load_pem_public_key(serialized_public_server, backend=default_backend())
-#     x_pub = format(pubkey.public_numbers().x, '064x')
-#     y_pub = format(pubkey.public_numbers().y, '064x')
-
-#     pub_key_attribute_dict = {
-#             'KTY': 'EC2',
-#             'CURVE': 'P_256',
-#             'ALG': 'ES256',
-#             EC2KpX : unhexlify(x_pub),
-#             EC2KpY : unhexlify(y_pub)
-#     }
-#     pub_cose_key = CoseKey.from_dict(pub_key_attribute_dict)
-
-#     decoded = CoseMessage.decode(packet)
-#     decoded.key = pub_cose_key
-
-#     if decoded.verify_signature() :
-#         #print("Signature is correct")
-#         return True
-#     else :
-#         #print("Signature is not correct")
-#         return False
-
-
 # Counter Signature version
 async def sign(encrypted):
     privkey = serialization.load_pem_private_key(serialized_private, password=None, backend=default_backend())
     bytes_key_priv = privkey.private_numbers().private_value.to_bytes(32, 'big')
     x = format(privkey.private_numbers().public_numbers.x, '064x')
     y = format(privkey.private_numbers().public_numbers.y, '064x')
-    
+
     msg2 = Countersign0Message(
         phdr = {Algorithm: Es256},
         #payload = 'signed message'.encode('utf-8')
@@ -237,93 +187,76 @@ async def decrypt(packet, key):
     decoded.key = cose_key_dec
     decrypt = decoded.decrypt()
     decrypt_decode = decrypt.decode("utf-8")
-    #print("Payload :", decrypt_decode) 
+    #print("Payload :", decrypt_decode)
 
     return decrypt_decode
 
 
-class EchoClientProtocol:
-    def __init__(self, message, loop):
-        self.message = message
-        self.loop = loop
-        self.transport = None
+async def run():
 
-    def connection_made(self, transport):
-        loop = asyncio.get_event_loop()
-        loop.create_task(self.connection_made_async(transport)) 
+    message = input('Message payload : ')
 
-    async def connection_made_async(self, transport):
-        self.transport = transport
-        #print('Send:', self.message)
+    key = await generate_key_sym()
 
-        key = await generate_key_sym()
+    encrypted = await encrypt(message, key)
 
-        encrypted = await encrypt(self.message, key)
+    packet = await sign(encrypted)
 
-        packet = await sign(encrypted)
+    reader, writer = await open_serial_connection(url='/dev/serial0', baudrate=115200)
 
-        self.transport.sendto(packet)
+    packet_hex = hexlify(packet)
+    # print(message)
+    writer.write(packet_hex + b'\n')
+    await writer.drain()
 
+    line = await reader.readline()
+    line = line[:-1]
+    # print("cleaned :", line)
+    line_unhex = unhexlify(line)
+    print("received :", line_unhex)
+    # print(line_unhex == packet)
 
-    def datagram_received(self, data, addr):
-        loop = asyncio.get_event_loop()
-        loop.create_task(self.datagram_received_async(data, addr))
-        
-    
-    async def datagram_received_async(self, data, addr):
-        #print("Received:", data.decode())
-        print("Received:", data)
+    try :
+        if line_unhex != b'' and b'error' not in line_unhex :
+            signature = await check_signature(line_unhex, serialized_public_server)
 
-        signature = await check_signature(data, serialized_public_server)
-
-        if signature :
-            print("Signature is correct")
-            key = await generate_key_sym()
-            decrypted = await decrypt(data, key)
-            print("Payload :", decrypted)
-        else :
-            print("Signature is not correct")
-
-        print("Close the socket")
-        self.transport.close()
-        
-
-    def error_received(self, exc):
-        print('Error received:', exc)
-
-    def connection_lost(self, exc):
-        print("Socket closed, stop the event loop")
-        loop = asyncio.get_event_loop()
-        loop.stop()
+            if signature :
+                print("Signature is correct")
+                key = await generate_key_sym()
+                decrypted = await decrypt(line_unhex, key)
+                print("Payload :", decrypted)
+            else :
+                print("Signature is not correct")
 
 
-async def start(add, port):
-    loop = asyncio.get_event_loop()
-    return await loop.create_datagram_endpoint(
-        lambda: EchoClientProtocol(message, loop),
-        remote_addr=(add, port))
+    except ValueError :
+        print("ValueError")
+        print('error, corrupted data')
+    except TypeError :
+        print("TypeError")
+        print('error, corrupted data')
+    except AttributeError :
+        print("AttributeError")
+        print('error, corrupted data')
+    except SystemError :
+        print("SystemError")
+        print('error, corrupted data')
 
 
-def main(add=add, port=port):
-    print(f'Connecting to server on port : {port}')
+    return
 
+
+def main():
     loop = asyncio.get_event_loop()
 
-    # connect = loop.create_datagram_endpoint(
-    #     lambda: EchoClientProtocol(message, loop),
-    #     remote_addr=(add, port))
-    con = start(add, port)
-    transport, _ = loop.run_until_complete(con)
+    # loop.run_until_complete(start())
 
-    # loop.run_forever()
-    # transport.close()
-    # loop.close()
     try:
-        loop.run_forever()
+        # loop.run_forever()
+        loop.run_until_complete(run())
     except KeyboardInterrupt:
         pass
 
-    transport.close()
     loop.close()
 
 
@@ -334,6 +267,7 @@ def get_key_by_value(d, reverse, value):
                reverse[_v] = _k
                break
     return reverse[value]
+
 
 if __name__ == '__main__':
     main()
---
+++ pycom/uart_test.py
@@ -0,0 +1,31 @@
+from machine import UART
+import time
+import ubinascii
+
+# this uses the UART_1 default pins for TXD and RXD (``P3`` and ``P4``)
+uart = UART(1, baudrate=115200)
+
+message = b'\xd0\x83XF\xa3\x01\x01\x05X\x1a000102030405060708090a0b0c\x00x#["0011", "0", "0x1145f03880d8a975"]\xa1\x0b\x83C\xa1\x01&\xa0X@\x92\xc9\xb2v\xa4\xaa\xd3s+\x8a\xebT\xdc\x07o\xf5NH\xe8 Rz4\x82\xe3H\x18\x97\x15\xb6Z\x1c\x97$X\\H\xd8\x88/aC\x15\x9d\x07\x93\x1ftG\xd1\xa2T\xb5\x9eB\xe0^J\xcb\x82\xd8\xccN\rUk\xe5(J\x13\rz\xf3\xf7\xbe\xb3\xa7\xc4\x88\xc1\xe9\xbf\xaaM\xc8i'
+
+while True :
+    # uart.write(b'hello')
+    # uart.write('\n')
+    # buffer = uart.read(5) # read up to 5 bytes
+    buffer = uart.readline()
+    print(buffer)
+    if buffer != None :
+        buffer = buffer[:-1]
+        print(buffer)
+        buffer_unhex = ubinascii.unhexlify(buffer)
+        print(buffer_unhex)
+        print(buffer_unhex == message)
+
+        # send buffer_unhex via lora
+
+        # get the response
+
+        # send the response back via uart
+
+        uart.write(buffer + b'\n')
+        # uart.write(b'\n')
+    time.sleep(5)
